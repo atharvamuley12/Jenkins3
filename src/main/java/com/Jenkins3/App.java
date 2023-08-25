b105f7327a5bb77210fba32af08f02cfc77abdc3
atharvamuley12 <atharvamuley12@gmail.com>
2023-08-25 20:13:44 +0530

    Jenkins3

---
+++ src/main/java/com/Jenkins3/App.java
@@ -0,0 +1,13 @@
+package com.Jenkins3;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! Welcome to Devops and i am using Jenkins" );
+    }
+}
